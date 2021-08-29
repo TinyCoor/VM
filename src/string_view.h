4cd60901e38158da3fca6b05042507e231178025
Yangguanping <rowbert121@outlook.com>
2021-08-29 17:13:06 +0800

    [改进]:don not export unused function to user 作者:yanngguanping

--- src/string_view.h
+++ src/string_view.h
@@ -14,11 +14,11 @@ typedef struct{
   const char *data;
 }string_view;
 
+#define SV_FORMAT(sv) (int)sv.count,sv.data
+
 string_view cstr_as_string_view(const char* cstr);
 string_view sv_chop_by_delim(string_view* sv,char c);
 string_view sv_trim(string_view sv);
-string_view sv_trim_left(string_view sv);
-string_view sv_trim_right(string_view sv);
 int sv_to_int(string_view view);
 bool sv_eq(string_view lhs,string_view rhs);
 
