59be7fcb62870ac0f7aba9d53803995253f0dffd
Yangguanping <rowbert121@outlook.com>
2021-08-29 15:46:31 +0800

    [改进]:refractor code in label_table rename unresolved_table to deffered table and push_label to resolve lable 作者:yanngguanping

--- src/label.c
+++ src/label.c
@@ -9,43 +9,51 @@
 #include <string.h>
 #include <ctype.h>
 
-int label_table_find_addr(const label_table* lt,string_view name){
-  for (size_t i = 0; i <lt->label_size ; ++i) {
-    if (sv_eq(lt->labels[i].name,name)){
-      return lt->labels[i].addr;
+int label_table_resolve_label(const label_table *lt,
+                              string_view name,
+                              Word *output) {
+    for (size_t i = 0; i < lt->label_size; ++i) {
+        if (sv_eq(lt->labels[i].name, name)) {
+            *output = lt->labels[i].word;
+            return 1;
+        }
     }
-  }
-  fprintf(stderr,"ERROR: label %.*s does not exist\n",
-          (int)name.count,name.data);
-  exit(-1);
-  return -1;
+    return 0;
 }
 
-void label_table_push_label(label_table* lt, string_view name,inst_addr addr){
-  assert(lt->label_size <LABEL_CAPACITY);
-  lt->labels[lt->label_size++] = (label_t){name,addr};
+int label_table_bind_label(label_table *lt,
+                           string_view name,
+                           Word word) {
+    assert(lt->label_size < LABEL_CAPACITY);
+    Word igonred = {0};
+    if (label_table_resolve_label(lt, name, &igonred)) {
+        return 0;
+    }
+    lt->labels[lt->label_size++] = (label_t) {name, word};
+    return 1;
 }
 
-void label_table_push_unresolved_label(label_table* lt,
-                                       string_view name,
-                                       inst_addr addr
-                                       ){
-  assert(lt->unresolved_size <UNRESOLVED_LABEL_CAPACITY);
-  lt->unresolved_labels[lt->unresolved_size++] = (unresolved_label){
-    name,
-    addr
-  };
+void label_table_push_deferred_label(label_table *lt,
+                                     string_view name,
+                                     inst_addr addr
+) {
+    assert(lt->deferred_size < UNRESOLVED_LABEL_CAPACITY);
+    lt->deferred_labels[lt->deferred_size++] = (deferred_label) {
+            name,
+            addr
+    };
 }
 
 
-void print_label_table(label_table* lt){
-  printf("Labels:\n");
-  for (int i = 0; i <lt->label_size ; ++i) {
-    printf("%.*s ->%lld\n",(int)lt->labels[i].name.count,lt->labels[i].name.data,lt->labels[i].addr);
-  }
+void print_label_table(label_table *lt) {
+    printf("Labels:\n");
+    for (int i = 0; i < lt->label_size; ++i) {
+        printf("%.*s ->%lld\n", (int) lt->labels[i].name.count, lt->labels[i].name.data, lt->labels[i].word.as_i64);
+    }
 
-  printf("Unresolved Labels:\n");
-  for (int i = 0; i <lt->unresolved_size ; ++i) {
-    printf("%lld ->%.*s\n",lt->unresolved_labels[i].addr,(int)lt->unresolved_labels[i].name.count,lt->unresolved_labels[i].name.data);
-  }
+    printf("deferred Labels:\n");
+    for (int i = 0; i < lt->deferred_size; ++i) {
+        printf("%lld ->%.*s\n", lt->deferred_labels[i].addr, (int) lt->deferred_labels[i].name.count,
+               lt->deferred_labels[i].name.data);
+    }
 }
--- src/label.h
+++ src/label.h
@@ -5,43 +5,43 @@
 #ifndef VM_SRC_LABEL_H
 #define VM_SRC_LABEL_H
 #include "string_view.h"
+#include "inst_t.h"
 
 #define LABEL_CAPACITY 1024
 #define UNRESOLVED_LABEL_CAPACITY 1024
 typedef uint64_t inst_addr;
 typedef struct {
   string_view name;
-  inst_addr addr;
+  Word word;
 }label_t;
 
 typedef struct {
   string_view name;
   inst_addr addr;
-}unresolved_label;
+}deferred_label;
 
 typedef struct {
   label_t labels[LABEL_CAPACITY];
   size_t label_size;
-  unresolved_label unresolved_labels[UNRESOLVED_LABEL_CAPACITY];
-  size_t unresolved_size;
+  deferred_label deferred_labels[UNRESOLVED_LABEL_CAPACITY];
+  size_t deferred_size;
 }label_table;
 
-int label_table_find_addr(const label_table*,
-                     string_view name);
+int label_table_resolve_label(const label_table*,
+                     string_view name,
+                     Word* );
 
 //TODO label should refered to Word instead of inst_addr
-void label_table_push_label(label_table*,
+int label_table_bind_label(label_table*,
                       string_view ,
-                      inst_addr addr);
+                      Word);
 
-void label_table_push_unresolved_label(label_table*,
+void label_table_push_deferred_label(label_table*,
                                        string_view,
                                        inst_addr addr);
 
 void print_label_table(label_table* lt);
 
-bool is_contains_unresolved_label(label_table*);
-
-
+bool is_contains_deffered_label(label_table*);
 
 #endif //VM_SRC_LABEL_H
