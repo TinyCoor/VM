7755c8eb97190c6da2cab76989935a830791ae17
Yangguanping <rowbert121@outlook.com>
2021-08-30 23:17:46 +0800

    [新功能]:add instruction for mem access 作者:yanngguanping

--- src/inst_t.c
+++ src/inst_t.c
@@ -3,84 +3,102 @@
 //
 #include "inst_t.h"
 #include "assert.h"
-int names_to_type(string_view inst_name,inst_t* out){
+bool names_to_type(string_view inst_name,inst_t* out){
     for (inst_t type = (inst_t)0; type < AMOUNT_OF_INSTS;type +=1){
         if (sv_eq(cstr_as_string_view(inst_names(type)),inst_name)){
             *out = type;
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
 
 const char* inst_names(inst_t type){
   switch (type) {
-    case INST_NOP:      {return "nop";}
-    case INST_PUSH:     {return "push";}
-    case INST_ADDI:     {return "addi";}
-    case INST_MULI:     {return "muli";}
-    case INST_DIVI:     {return "divi";}
-    case INST_SUBI:     {return "subi";}
-    case INST_ADDF:     {return "addf";}
-    case INST_MULF:     {return "mulf";}
-    case INST_DIVF:     {return "divf";}
-    case INST_SUBF:     {return "subf";}
-    case INST_NOT:      {return "not";}
-    case INST_JMP :     {return "jmp";}
-    case INST_HALT:     {return "halt";}
-    case INST_JMP_IF:   {return "jmp_if";}
-    case INST_EQ:       {return "eq";}
-    case INST_DROP:     {return "drop";}
-    case INST_GEF:      {return "gef";}
-    case INST_CALL:     {return "call";}
-    case INST_SWAP:     {return "swap";}
-    case INST_FFI:      {return "ffi";}
-    case INST_DUP:      {return "dup";}
-    case INST_ANDB:     {return "and";}
-    case INST_ORB:      {return "or";}
-    case INST_SHR:      {return "shr";}
-    case INST_SHL:      {return "shl";}
-    case INST_XORB:     {return "xor";}
-    case INST_RET:      {return "ret";}
-    case AMOUNT_OF_INSTS:
-    default:assert(0 && "inst_type_as_cstr:Unreachable");
+        case INST_NOP:      {return "nop";}
+        case INST_PUSH:     {return "push";}
+        case INST_ADDI:     {return "addi";}
+        case INST_MULI:     {return "muli";}
+        case INST_DIVI:     {return "divi";}
+        case INST_SUBI:     {return "subi";}
+        case INST_ADDF:     {return "addf";}
+        case INST_MULF:     {return "mulf";}
+        case INST_DIVF:     {return "divf";}
+        case INST_SUBF:     {return "subf";}
+        case INST_NOT:      {return "not";}
+        case INST_JMP :     {return "jmp";}
+        case INST_HALT:     {return "halt";}
+        case INST_JMP_IF:   {return "jmp_if";}
+        case INST_EQ:       {return "eq";}
+        case INST_DROP:     {return "drop";}
+        case INST_GEF:      {return "gef";}
+        case INST_CALL:     {return "call";}
+        case INST_SWAP:     {return "swap";}
+        case INST_FFI:      {return "ffi";}
+        case INST_DUP:      {return "dup";}
+        case INST_ANDB:     {return "and";}
+        case INST_ORB:      {return "or";}
+        case INST_SHR:      {return "shr";}
+        case INST_SHL:      {return "shl";}
+        case INST_XORB:     {return "xor";}
+        case INST_RET:      {return "ret";}
+        case INST_NOTB:     {return "notb";}
+        case INST_READ8:{return "read8";}
+        case INST_READ16:{return "read16";}
+        case INST_READ32:{return "read32";}
+        case INST_READ64:{return "read64";}
+        case INST_WRITE8:{return "write8";}
+        case INST_WRITE16:{return "write16";}
+        case INST_WRITE32:{return "write32";}
+        case INST_WRITE64:{return "write64";}
+        case AMOUNT_OF_INSTS:
+        default:assert(0 && "inst_type_as_cstr:Unreachable");
   }
     return "";
 }
 
-int inst_has_op(inst_t type){
+bool inst_has_op(inst_t type){
   switch (type) {
-        case INST_NOP:{return 0;}
-        case INST_ADDI:{return 0;}
-        case INST_SUBI:{return 0;}
-        case INST_MULI:{return 0;}
-        case INST_SUBF:{return 0;}
-        case INST_MULF:{return 0;}
-        case INST_DIVF:{return 0;}
-        case INST_HALT:{return 0;}
-        case INST_RET:{return 0;}
-        case AMOUNT_OF_INSTS:{return 0;}
-        case INST_DROP:{return 0;}
-        case INST_ADDF:{return 0;}
-        case INST_DIVI:{return 0;}
-        case INST_EQ:{return 0;}
-        case INST_GEF:{return 0;}
-        case INST_NOT:{return 0;}
-        case INST_PUSH: {return 1;}
-        case INST_JMP:  {return 1;}
-        case INST_JMP_IF:{return 1;}
-        case INST_DUP:{return 1;}
-        case INST_SWAP:{return 1;}
-        case INST_CALL:{return 1;}
-        case INST_FFI:{return 1;}
-        case INST_ANDB:{return 0;}
-        case INST_ORB:{return 0;}
-        case INST_XORB:{return 0;}
-        case INST_SHL:{return 0;}
-        case INST_SHR:{return 0;}
+        case INST_NOP:{return false;}
+        case INST_ADDI:{return false;}
+        case INST_SUBI:{return false;}
+        case INST_MULI:{return false;}
+        case INST_SUBF:{return false;}
+        case INST_MULF:{return false;}
+        case INST_DIVF:{return false;}
+        case INST_HALT:{return false;}
+        case INST_RET:{return false;}
+        case INST_DROP:{return false;}
+        case INST_ADDF:{return false;}
+        case INST_DIVI:{return false;}
+        case INST_EQ:{return false;}
+        case INST_GEF:{return false;}
+        case INST_NOT:{return false;}
+        case INST_PUSH: {return true;}
+        case INST_JMP:  {return true;}
+        case INST_JMP_IF:{return true;}
+        case INST_DUP:{return true;}
+        case INST_SWAP:{return true;}
+        case INST_CALL:{return true;}
+        case INST_FFI:{return true;}
+        case INST_ANDB:{return false;}
+        case INST_ORB:{return false;}
+        case INST_XORB:{return false;}
+        case INST_SHL:{return false;}
+        case INST_SHR:{return false;}
+        case INST_NOTB:{return false;}
+      case INST_READ8:{return false;}
+      case INST_READ16:{return false;}
+      case INST_READ32:{return false;}
+      case INST_READ64:{return false;}
+      case INST_WRITE8:{return false;}
+      case INST_WRITE16:{return false;}
+      case INST_WRITE32:{return false;}
+      case INST_WRITE64:{return false;}
+      case AMOUNT_OF_INSTS:{return false;}
         default:{
-          return 0;
+          return false;
         }
   }
-    return 0;
+    return false;
 }
--- src/inst_t.h
+++ src/inst_t.h
@@ -4,60 +4,72 @@
 
 #ifndef VM_SRC_INST_T_H_
 #define VM_SRC_INST_T_H_
+
 #include "string_view.h"
 #include <stdint.h>
 #include <assert.h>
 
 typedef union {
-  uint64_t as_u64;
-  int64_t as_i64;
-  double as_f64;
-  void *as_ptr;
-}Word;
+    uint64_t as_u64;
+    int64_t as_i64;
+    double as_f64;
+    void *as_ptr;
+} Word;
 
-static_assert(sizeof(Word)==8,"Only support x64 Arch");
+static_assert(sizeof(Word) == 8, "Only support x64 Arch");
 
 //TODO impl more inst 
 // converter needed  interger -> float float->integer
 typedef enum {
-  INST_NOP = 0,
-  INST_PUSH,
-  INST_ADDI,
-  INST_SUBI,
-  INST_MULI,
-  INST_DIVI,
-  INST_ADDF,
-  INST_SUBF,
-  INST_MULF,
-  INST_DIVF,
-  INST_JMP,
-  INST_JMP_IF,
-  INST_EQ,
-  INST_DUP,
-  INST_NOT,
-  INST_HALT,
-  INST_SWAP,
-  INST_GEF,
-  INST_RET,
-  INST_DROP,
-  INST_CALL,
-  INST_FFI,
-  INST_ANDB,
-  INST_ORB,
-  INST_XORB,
-  INST_SHL,
-  INST_SHR,
-  AMOUNT_OF_INSTS,
+    INST_NOP = 0,
+    INST_PUSH,
+    INST_ADDI,
+    INST_SUBI,
+    INST_MULI,
+    INST_DIVI,
+    INST_ADDF,
+    INST_SUBF,
+    INST_MULF,
+    INST_DIVF,
+    INST_JMP,
+    INST_JMP_IF,
+    INST_EQ,
+    INST_DUP,
+    INST_NOT,
+    INST_HALT,
+    INST_SWAP,
+    INST_GEF,
+    INST_RET,
+    INST_DROP,
+    INST_CALL,
+    INST_FFI,
+    INST_ANDB,
+    INST_ORB,
+    INST_XORB,
+    INST_SHL,
+    INST_SHR,
+    INST_NOTB,
+    INST_READ8,
+    INST_READ16,
+    INST_READ32,
+    INST_READ64,
+    INST_WRITE8,
+    INST_WRITE16,
+    INST_WRITE32,
+    INST_WRITE64,
+    AMOUNT_OF_INSTS,
 } inst_t;
 
 typedef struct {
-  inst_t type;
-  Word operand;
+    inst_t type;
+    Word operand;
 } inst;
 
 
-const char* inst_names(inst_t type);
-int  names_to_type(string_view inst_name,inst_t* type);
+const char *inst_names(inst_t type);
 
-int inst_has_op(inst_t type);
+bool names_to_type(string_view inst_name, inst_t *type);
+
+bool inst_has_op(inst_t type);
+
 #endif //VM_SRC_INST_T_H_
