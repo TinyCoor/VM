e300e6045b023fc5f4bac014dc4accb4e1161e40
Yangguanping <rowbert121@outlook.com>
2021-08-30 23:13:39 +0800

    [新功能]:add shell script for build 作者:yanngguanping

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -xe
+
+CC=${CC:=/usr/bin/cc}
+CFLAGS = "-Wall -Wextra -Wswitch-enum -fno-strict-aliasing -pedantic -Wmissing-prototypes  -ggdb -std=c11 -O0 "
+LIB_SRC ="./src/inst.c ./src/vm.c ./src/label.c ./src/string_view.c
+./src/error_t.c ./src/file.c ./src/native_call.c ./src/utils.c"
+LIBS =
+$CC $CFLAGS -o basm ./src/basm.c ${LIB_SRC}   $LIBS
+$CC $CFLAGS -o bme ./src/bmi.c ${LIB_SRC} $LIBS
+$CC $CFLAGS -o debasm ./src/disassmebler.c ${LIB_SRC}  $LIBS
+
+for example in `find examples/ -name \*.bm | sed "s/\.vbm//"`; do
+    ./basm "$example.vm" "$example.vbm"
+done
